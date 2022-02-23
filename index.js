da0813b554ac59775c7619570c635bedb7eddc07
Kevin <rojke34@gmail.com>
2022-02-23 17:55:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(jsonParser);
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-let port = process.env.PORT || 443;
+let port = process.env.PORT || 3000;
 const importData = require("./data.json");
 
 app.use(cors());
